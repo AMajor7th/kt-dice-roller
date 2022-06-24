71b87926d39c57cd35231d7b73c3d3931a60abf3
Khôi Nguyễn <anhkhoi301205@gmail.com>
2022-06-24 13:37:11 +0700

    clean code

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -5,15 +5,19 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            rollDice()
-        }
+        rollButton.setOnClickListener { rollDice() }
     }
+
+    //this method create a dice, roll it and update the text view
     private fun rollDice() {
         val dice = Dice(6)
         val resultTextView: TextView = findViewById(R.id.textView)
