0727cd52c9af47e326e2cf7d0faa83efc4ea6f50
Khôi Nguyễn <anhkhoi301205@gmail.com>
2022-06-24 15:58:47 +0700

    add image, final touches

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -15,13 +15,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
+        rollDice()
     }
 
-    //this method create a dice, roll it and update the text view
+    //this method create a dice, roll it and update the image
     private fun rollDice() {
-        val dice = Dice(6)
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = dice.roll().toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val numRolled = Dice(6).roll()
+        val imageResource = when (numRolled) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(imageResource)
+        diceImage.contentDescription = numRolled.toString()
     }
 }
 
